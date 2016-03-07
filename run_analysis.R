a98e33937b95652138ae1297252ea222073025d2
greenorange04 <m-zolotko@yandex.ru>
2016-03-07 01:01:30 +0100

    getting and cleaning data

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,5 @@
-library(dplyr)
+library(dplyr) 
+library(tidyr)
 features <- read.table("features.txt", stringsAsFactors = FALSE)
 feat_test <- read.table("test/X_test.txt")
 act_test <- read.table("test/y_test.txt")
@@ -7,19 +8,17 @@ act_train <- read.table("train/y_train.txt")
 subject_train <- read.table("train/subject_train.txt")
 subject_test <- read.table("test/subject_test.txt")
 act_labels <- read.table("activity_labels.txt")
-feat_test <- tbl_df(feat_test)
-act_test <- tbl_df(act_test)
-act_labels <- tbl_df(act_labels)
+
 actnames_test <- merge(act_test, act_labels, by.x = "V1", by.y = "V1")
 actnames_train <- merge(act_train, act_labels, by.x = "V1", by.y = "V1")
-actnames_test <- tbl_df(actnames_test)
+
 join_test <- bind_cols(as.data.frame(actnames_test$V2), as.data.frame(subject_test$V1), feat_test)
 join_train <- bind_cols(as.data.frame(actnames_train$V2), as.data.frame(subject_train$V1), feat_train)
 names(join_test) <- c("activity", "subject", features$V2)
 names(join_train) <- c("activity", "subject", features$V2)
 join <- bind_rows(join_test, join_train)
 
-#names(join) <- c("activity", "subject", features$V2)
+
 sel_join <- join[,c(1, 2, grep(".*mean\\(.*|.*std\\(.*", names(join)))]
 names(sel_join) <- gsub("-", "", names(sel_join))
 names(sel_join) <- gsub("(.*)(mean\\(\\))(.*)", "\\1\\3_mean", names(sel_join))
@@ -31,5 +30,5 @@ sel_join_spr <- spread(sel_join_sep, metric, value)
 
 grouped_dataset <- group_by(sel_join_spr, activity, subject, feature)
 sum_dataset <- mutate(grouped_dataset, avg_mean = mean(mean), avg_std = mean(std))
-sum_dataset2 <- summarize(grouped_dataset, avg_mean = mean(mean), avg_std = mean(std))
+
 write.table(sum_dataset, file = "final_dataset.txt", row.name = FALSE)
